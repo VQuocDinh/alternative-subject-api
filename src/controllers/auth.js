64e2e7f133e43c664b8f12ecd7298becf8cd83fd
voquocdinh <vqdinh2202@@gmail.com>
2024-11-27 23:19:08 +0700

    format source

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,37 +1,17 @@
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import db from "../models/index.js";
-
-const hashPassword = (password) => {
-  const salt = bcrypt.genSaltSync(10);
-  return bcrypt.hashSync(password, salt);
-};
+import AuthService from "../service/auth";
 
 const register = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const existingUser = await db.staff.findOne({
-      where: { email: email },
-    });
-    if (existingUser) {
-      return res.status(400).json({
-        success: false,
-        message: "Email already has an account.",
-      });
-    }
-
-    const hashedPassword = hashPassword(password);
-    await db.staff.create({ email: email, password: hashedPassword });
-
+    await AuthService.registerUser(email, password);
     return res.status(200).json({
       success: true,
-      message: "Account registered successfully.",
+      message: "Account registered successfully."
     });
   } catch (error) {
-    return res.status(500).json({
+    return res.status(400).json({
       success: false,
-      message: "An error occurred while registering the account.",
-      error: error.message,
+      message: error.message || "An error occurred while registering the account."
     });
   }
 };
@@ -39,42 +19,16 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await db.staff.findOne({
-      where: { email: email },
-    });
-    if (!user) {
-      return res.status(400).json({
-        success: false,
-        message: "The account is not registered. Please check again.",
-      });
-    }
-
-    const isMatch = bcrypt.compareSync(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({
-        success: false,
-        message: "Incorrect password.",
-      });
-    }
-
-    const token = jwt.sign(
-      { id: user.staff_id, role: user.role_id },
-      process.env.JWT_SECRET || "secretkey",
-      { expiresIn: "1h" }
-    );
-
+    const userData = await AuthService.loginUser(email, password);
     return res.status(200).json({
       success: true,
-      token: token,
-      role_id: user.role_id,
-      staffId: user.staff_id,
-      message: "Login is successful.",
+      ...userData,
+      message: "Login is successful."
     });
   } catch (error) {
-    return res.status(500).json({
+    return res.status(400).json({
       success: false,
-      message: "An error occurred while logging in.",
-      error: error.message,
+      message: error.message || "An error occurred while logging in."
     });
   }
 };
---
+++ src/service/auth.js
@@ -0,0 +1,54 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import db from "../models/index.js";
+
+  const hashPassword = (password) => {
+    const salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password, salt);
+  }
+
+  const registerUser = async(email, password) => {
+    const existingUser = await db.staff.findOne({
+      where: { email }
+    });
+
+    if (existingUser) {
+      throw new Error("Email already has an account.");
+    }
+
+    const hashedPassword = this.hashPassword(password);
+    await db.staff.create({ 
+      email, 
+      password: hashedPassword 
+    });
+  }
+
+  const loginUser = async (email, password) => {
+    const user = await db.staff.findOne({
+      where: { email }
+    });
+
+    if (!user) {
+      throw new Error("The account is not registered. Please check again.");
+    }
+
+    const isMatch = bcrypt.compareSync(password, user.password);
+    if (!isMatch) {
+      throw new Error("Incorrect password.");
+    }
+
+    const token = jwt.sign(
+      { id: user.staff_id, role: user.role_id },
+      process.env.JWT_SECRET || "secretkey",
+      { expiresIn: "1h" }
+    );
+
+    return {
+      token,
+      role_id: user.role_id,
+      staffId: user.staff_id
+    };
+  }
+
+
+export {loginUser, registerUser};
