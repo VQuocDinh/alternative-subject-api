b95d560f2c2c7b80e4bc4c29b949811698477327
ndtien142 <ndtien142@gmail.com>
2024-12-03 18:45:23 +0700

    config swagger

--- src/routes/account.js
+++ src/routes/account.js
@@ -1,6 +1,24 @@
-import * as controllers from '../controllers'
-import express from 'express'
+import * as controllers from '../controllers';
+import express from 'express';
 
-const router =express.Router()
-router.get('/get', controllers.getAccount)
-export default router
+/**
+ * @swagger
+ * /get:
+ *   get:
+ *     summary: Retrieve account information
+ *     tags: [Accounts]
+ *     responses:
+ *       200:
+ *         description: Account data retrieved successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Account data retrieved successfully
+ */
+const router = express.Router();
+router.get('/get', controllers.getAccount);
+export default router;
