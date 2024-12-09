b5854a70e99451aa594a530b1a21b4308152dabb
ndtien142 <ndtien142@gmail.com>
2024-12-09 22:14:44 +0700

    status medical record

--- src/common/common.constant.js
+++ src/common/common.constant.js
@@ -4,3 +4,16 @@ const ROLE = Object.freeze({
   NURSE: 'nurse',
   PATIENT: 'patient',
 });
+
+const MEDICAL_RECORD_STATUS = Object.freeze({
+  ALL: 'all',
+  CREATED: 'created', // Bệnh án được tạo
+  REGISTERED: 'registered', // Đăng ký khám bệnh
+  NURSE_RECEIVED: 'nurse_received', // Y tá tiếp nhận
+  DOCTOR_RECEIVED: 'doctor_received', // Bác sĩ tiếp nhận
+  WAITING_LAB: 'waiting_lab', // Chờ xét nghiệm
+  WAITING_PAYMENT: 'waiting_payment', // Chờ thanh toán
+  COMPLETED: 'completed', // Hoàn thành khám bệnh
+});
+
+export { ROLE, MEDICAL_RECORD_STATUS };
